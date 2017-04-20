c57d75166d40095d1ce34bc5df02c99f8811f4f6
Eva0410 <evapuermayr@gmail.com>
2017-04-20 15:34:19 +0200

    Added Folders

---
+++ SourceCode/OpticianMgr/OpticianMgr.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpticianMgr.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //TODO add further DBSets
+        //public DbSet<Tutor> Tutors { get; set; }
+
+
+        public ApplicationDbContext() : base("name=DefaultConnection")
+        {
+
+        }
+
+        public ApplicationDbContext(string nameOrConnectionString) : base(nameOrConnectionString)
+        {
+
+        }
+    }
+}
---
+++ SourceCode/OpticianMgr/OpticiatnMgr.Core/Contracts/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpticiatnMgr.Core.Contracts
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        //TODO  add further repositories
+
+        //IGenericRepository<Tutor> TutorRepository { get; }
+
+        void Save();
+
+        void DeleteDatabase();
+    }
+}
