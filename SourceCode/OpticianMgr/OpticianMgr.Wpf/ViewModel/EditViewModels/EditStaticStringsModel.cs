9087030762b8422850db8cc9db0af5fd7cd25c6c
Eva0410 <evapuermayr@gmail.com>
2018-03-11 23:08:16 +0100

    Feedback beim Schreibn umgesetzt

--- SourceCode/OpticianMgr/OpticianMgr.Wpf/ViewModel/EditViewModels/EditStaticStringsModel.cs
+++ SourceCode/OpticianMgr/OpticianMgr.Wpf/ViewModel/EditViewModels/EditStaticStringsModel.cs
@@ -11,13 +11,13 @@ namespace OpticianMgr.Wpf.ViewModel
     {
 
         public event EventHandler<EventArgs> CloseRequested;
-        public static string SingleEmailSubjectGlassesOrder { get { return Properties.Settings.Default.SingleEmailSubjectGlassesOrder; } set { Properties.Settings.Default.SingleEmailSubjectGlassesOrder = value; } }
-        public static string SingleEmailTextGlassesOrder { get { return Properties.Settings.Default.SingleEmailTextGlassesOrder; } set { Properties.Settings.Default.SingleEmailTextGlassesOrder = value; } }
-        public static string SingleEMailSubjectContactLensOrder { get { return Properties.Settings.Default.SingleEMailSubjectContactLensOrder; } set { Properties.Settings.Default.SingleEMailSubjectContactLensOrder = value; } }
-        public static string SingleEmailTextContactLensOrder { get { return Properties.Settings.Default.SingleEmailTextContactLensOrder; } set { Properties.Settings.Default.SingleEmailTextContactLensOrder = value; } }
-        public static string SingleSMSTextGlassesOrder { get { return Properties.Settings.Default.SingleSMSTextGlassesOrder; } set { Properties.Settings.Default.SingleSMSTextGlassesOrder = value; } }
-        public static string SingleSMSTextContactLensOrder { get { return Properties.Settings.Default.SingleSMSTextContactLensOrder; } set { Properties.Settings.Default.SingleSMSTextContactLensOrder = value; } }
-        public static string SmsSenderText { get { return Properties.Settings.Default.SmsSenderText; } set { Properties.Settings.Default.SmsSenderText = value; } }
+        public string SingleEmailSubjectGlassesOrder { get { return Properties.Settings.Default.SingleEmailSubjectGlassesOrder; } set { Properties.Settings.Default.SingleEmailSubjectGlassesOrder = value; } }
+        public string SingleEmailTextGlassesOrder { get { return Properties.Settings.Default.SingleEmailTextGlassesOrder; } set { Properties.Settings.Default.SingleEmailTextGlassesOrder = value; } }
+        public string SingleEMailSubjectContactLensOrder { get { return Properties.Settings.Default.SingleEMailSubjectContactLensOrder; } set { Properties.Settings.Default.SingleEMailSubjectContactLensOrder = value; } }
+        public string SingleEmailTextContactLensOrder { get { return Properties.Settings.Default.SingleEmailTextContactLensOrder; } set { Properties.Settings.Default.SingleEmailTextContactLensOrder = value; } }
+        public string SingleSMSTextGlassesOrder { get { return Properties.Settings.Default.SingleSMSTextGlassesOrder; } set { Properties.Settings.Default.SingleSMSTextGlassesOrder = value; } }
+        public string SingleSMSTextContactLensOrder { get { return Properties.Settings.Default.SingleSMSTextContactLensOrder; } set { Properties.Settings.Default.SingleSMSTextContactLensOrder = value; } }
+        public string SmsSenderText { get { return Properties.Settings.Default.SmsSenderText; } set { Properties.Settings.Default.SmsSenderText = value; } }
         public ICommand CloseCommand { get; set; }
         public ICommand SaveCommand { get; set; }
         public EditStaticStringsModel()
